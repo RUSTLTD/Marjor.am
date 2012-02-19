700d7a6d41043735802ca78ef42ef32ee4157095
Luke Noonan <luke.noonan@gmail.com>
2012-02-18 23:55:29 -0500

    added index.php and added comments to modules.php

--- core/classes/module.php
+++ core/classes/module.php
@@ -8,19 +8,35 @@
  */
 
 
-class module
-{
+class module{
+    /*
+    install() is called when the module is installed. This will happen 
+    either as part of Marjoram's installation or via the dashboard.
+    */
     public static install() {
         return true;
     }
+    
+    /*
+    init() is called at the start of every request cycle. It is used 
+    to determine and include requirements, and initialize class variables.
+    */
     public static init() {
         return true;
     }
-
+    
+    /*
+    bootstrap() is called after init() but before handeling the request. 
+    It is for debug purposes, and not recommended for production use. 
+    */
     public static bootstrap() {
         return true;
     }
     
+    /*
+    close() is called after the response is sent. It is used to close 
+    database connections and acts as a deconstructor.
+    */
     public static close() {
         return true;
     }
---
+++ index.php
@@ -0,0 +1,11 @@
+<? php
+/**
+ * Responsible for executing the main Marjoram state loop
+ *
+ * @license    Modified BSD License (see LICENSE file)
+ *
+ */
+ 
+ require_once('config.php');
+ 
+?>

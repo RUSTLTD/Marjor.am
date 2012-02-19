339dde089b5a22abb4ace3e9bd5ada4a4b7b5f2f
Lucas Miller <lucas@dasberet.com>
2012-02-18 23:49:53 -0500

    Created working copy of Localization module, outlines structure for Database module. Also added License file.

---
+++ core/modules/localization/localization.mod.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Class responsible for text translation and localization; Phrases are referenced using a look-up table and returned if a translation is found
+ *
+ * @license    Modified BSD License (see LICENSE file)
+ * @version    Release: 
+ * @link       
+ */
+	
+	
+    class Localization extends module{
+        public static install(){
+
+        }
+
+        public static init(){
+            if(!is_defined('LANGUAGE') || !LANGUAGE){
+                define('LANGUAGE','en');
+            }
+        }
+
+        public static translate($string){
+
+            /* 
+			If we find a line in the localization database for the given
+			language and text, return it; otherwise return the untranslated
+			string.
+			*/
+
+            if($replacement = Database::find(
+			    array(
+                    'from'=>'localization',
+                    'where'=>array(
+                        'language'=>LANGUAGE,
+                        'text'=>DB::escape($string)
+                    ),
+                    'limit'=>1
+                )
+			)){
+				return $replacement;
+			}
+			return $string; 
+        }
+    }
+
+	// Helper function with short name to call Localization translate function
+    function t($string){
+        return Localization::translate($string);
+    }
+?>

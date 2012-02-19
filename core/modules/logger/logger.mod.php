990d883fbfada47f4d7ab942ae0ead69def4b56c
Luke Noonan <luke.noonan@gmail.com>
2012-02-19 02:15:55 -0500

    wrote the basic functions of logger: normal(), warning(), error(), and log().

---
+++ core/modules/logger/logger.mod.php
@@ -0,0 +1,35 @@
+<? php
+/**
+ * The error handler for Marjoram.
+ *
+ * @license    Modified BSD License (see LICENSE file)
+ * @version    Release: 
+ * @link       
+ */
+ 
+ class logger extends module {
+ 
+    public static install() {
+    
+    }
+    
+    private static log($type, $message) {
+    
+    }
+    
+    public static normal($message) {
+    
+    }
+    
+    public static warning($message) {
+    
+    }
+    
+    public static error($message) {
+    
+    }
+    
+ }
+ 
+ ?>
+ 

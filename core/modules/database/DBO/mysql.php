9775d124656084a8a3e57a06804e82e5af74d6cc
Lucas Miller <lucas@dasberet.com>
2012-08-13 20:49:01 -0400

    Created 'Create Table' functionality for MySQL DBO

--- core/modules/database/DBO/mysql.php
+++ core/modules/database/DBO/mysql.php
@@ -112,7 +112,42 @@ class MYSQL extends DBO{
    }
    
    public function create_table($args){
-        $sql = '';
+        $sql = 'CREATE TABLE ';
+        
+        if(!isset($args['table']) && ((is_array($args['table']) && count($args['table'])>0) || is_string($args['table']))){
+            Logger::error('MYSQLDBO_MISSINGTABLE','Missing \'table\' attribute in create_table() query!');
+            return false;
+        }
+        
+        $sql .= "`{$args['table']}`";
+        
+        if(isset($args['fields']) && is_array($args['fields']) && count($args['fields'])>0){
+            $sql .= ' (';
+            foreach($args['fields'] as $field){
+                if(!isset($field['name']) || !isset($field['type'])){
+                    Logger::error('MYSQLDBO_IMPROPERFIELD','Improper field in create_table() query! [Field info: '.var_dump($field,true).']');
+                    return false;
+                }
+                $sql .= "`{$field['name']}` {$field['type']}";
+                if(isset($field['auto_increment']) && $field['auto_increment']===true){
+                    $sql .= ' AUTO_INCREMENT';
+                }
+                if(isset($field['unique']) && $field['unique']===true){
+                    $sql .= ' UNIQUE';
+                }
+                if(isset($field['primary']) && $field['primary']===false){
+                    $sql .= ' PRIMARY KEY';
+                }
+                if(isset($field['comment']) && $field['comment']!=""){
+                    $sql .= ' COMMENT "'.$this->escape($field['comment']).'"';
+                }
+                if(isset($field['default']) && $field['comment']!=""){
+                    $sql .= ' DEFAULT "'.$this->escape($field['default']).'"';
+                }
+            }
+        }
+        
+        
         
         return $this->query($sql);
    }
